c0e0fb38c12b44368c0d787eef6ed0ea29aa4e6d
beklour <bek.lourdes@gmail.com>
2024-10-30 17:57:41 -0300

    pag de show y links en el footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["**/*.{html,js}"],
+  content: [
+    'index.html',
+    'js/*.js',
+    'shows.html'
+  ],
   theme: {
     fontFamily: {
       gloria: ['Gloria Hallelujah'],
