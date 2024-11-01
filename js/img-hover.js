34a8b6a2f39445c62411f3fa3682983a815ff1de
santo-soi <santoiosi@hotmail.com>
2024-11-01 15:07:01 -0300

    nav bar mobile index y ultimo

--- js/img-hover.js
+++ js/img-hover.js
@@ -1,5 +1,4 @@
-let imagenes = document.querySelectorAll('#paginas img');
-
+const portada = document.querySelectorAll('img');
 const burguer = document.querySelector('#burguer');
 const desplegable = document.querySelector('#desplegable');
 
@@ -7,4 +6,18 @@ burguer.addEventListener('click', () =>{
     desplegable.classList.toggle('hidden');
     desplegable.classList.toggle('flex');
 })
+
+if (portada[0].src.includes('img/portada.jpg')) {
+    cambiarImagenPortada(portada[0]);
+}
+ 
+function cambiarImagenPortada(imagen) { 
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // Cambia el src cuando es mobile
+        imagen.src = "img/portada-mobile.jpg";
+    } else {
+        // Restablece el src cuando no es mobile
+        imagen.src = "img/portada.jpg";
+    }
+}
 
