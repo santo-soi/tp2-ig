0bd02804c9e3f1ba5f754e11f7c4098b7fd0cd12
santo-soi <santoiosi@hotmail.com>
2024-11-01 12:44:52 -0300

    menu hamburguesa

--- js/img-hover.js
+++ js/img-hover.js
@@ -1,1 +1,10 @@
 let imagenes = document.querySelectorAll('#paginas img');
+
+const burguer = document.querySelector('#burguer');
+const desplegable = document.querySelector('#desplegable');
+
+burguer.addEventListener('click', () =>{
+    desplegable.classList.toggle('hidden');
+    desplegable.classList.toggle('flex');
+})
+
