3c87c19c4567f595eeddb006396b904c1c184ec6
santo-soi <santoiosi@hotmail.com>
2024-11-01 19:41:28 -0300

    tipografias, correcion configuracion de tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
   theme: {
     fontFamily: {
       gloria: ['Gloria Hallelujah'],
-      moda: ['Bodoni Moda']
+      moda: ['Bodoni Moda'],
+      mm: ['Major Mono Display'],
+      julius: ['Julius Sans One']
 
     },
     extend: { 
@@ -33,34 +35,4 @@ module.exports = {
 }
 
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    'index.html',
-    'js/*.js',
-    'shows.html'
-  ],
-  theme: {
-    fontFamily: {
-      gloria: ['Gloria Hallelujah'],
-      moda: ['Bodoni Moda']
-
-    },
-    extend: { 
-       backgroundImage: {
-         portadaMobile: "url('../img/portada-mobile.jpg')",
-         portada: "url('../img/portada.jpg')"
-      },
-    colors: {
-      rojoPelado : '#DB2323',
-      rojoFondo :'#9f120d'
-    },
-    width: {
-      'bloquesMain': '25%'
-    }
-
-    },
-  },
-  plugins: [],
-}
 
