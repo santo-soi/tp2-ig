b172bcfbca678b7d57faccb1544e8cc5495ac3e3
santo-soi <santoiosi@hotmail.com>
2024-11-03 15:33:55 -0300

    ultimo responsive, img-hover por funcionalidades-base

--- js/funcionalidades-base.js
+++ js/funcionalidades-base.js
@@ -16,10 +16,11 @@ if (portada[0].src.includes('img/portada.jpg')) {
 if (vidCuandoCorre != null){
     vidCuandoCorre.addEventListener("click", () => {
         // Cambia el contenido del div por el iframe de YouTube
-        this.innerHTML += '<h3 class="text-rojoPelado">Vas a ser redirigido a youtube para ver el video...</h3>'
+        vidCuandoCorre.innerHTML += '<h3 class="text-rojoPelado">Vas a ser redirigido a youtube para ver el video...</h3>'
         setTimeout(() => window.location = 'https://www.youtube.com/watch?v=2xE8dzUc0hM', 1000); 
     });
 }
+
 
 if (verMas != null){
     const volverAocultar = document.querySelector('#mostrarmenos');
